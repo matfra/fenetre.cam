cf7774769b98624249915595298f5b001a417b5c
Mathieu Frappier <mathieu.frappier@gmail.com>
2025-07-03 18:27:27 -0700

    better (maybe) handling of http calls

--- gopro.py
+++ gopro.py
@@ -3,102 +3,37 @@ import time
 from typing import Optional
 
 
-def _set_gopro_settings(
-    ip_address: str,
-    preset: str,
-    timeout: int,
-    verify_path: str,
-    scheme: str,
+def _make_gopro_request(
+    url_path: str,
+    expected_response_code: int = 200,
+    expected_response_text: str = "{}",
+    ip_address: str = "10.5.5.9",
+    timeout: int = 5,
+    root_ca_filepath: str = "",
+    scheme: str = "http",
 ):
-    """Set the GoPro settings for remote control."""
-    # Takeover control of the GoPro camera https://gopro.github.io/OpenGoPro/http#tag/Control/operation/GPCAMERA_SYSTEM_RESET
-    camera_control_url = (
-        f"{scheme}://{ip_address}/gopro/camera/control/set_ui_controller?p=2"
-    )
-    requests.get(
-        camera_control_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # Set the control mode to pro
-    pro_mode_url = f"{scheme}://{ip_address}/gopro/camera/setting?option=1&setting=175"
-    requests.get(
-        pro_mode_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # TODO(Enable this based on the GPS coordinates of the camera and expected sunset time)
-
-    night_photo_url = (
-        f"{scheme}://{ip_address}/gopro/camera/setting?option=1&setting=177"
-    )
-    requests.get(
-        night_photo_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # Set the LCD brightness to minimum to avoid wearing out the LCD
-    lcd_brightness_url = f"{scheme}://{ip_address}/gopro/camera/setting?option=10&setting=88"
-    requests.get(
-        lcd_brightness_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # Set photo output to superphoto
-    photo_output_url = f"{scheme}://{ip_address}/gopro/camera/setting?option=3&setting=125"
-    requests.get(
-        photo_output_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # Turn off all LEDs:
-    led_off_url = f"{scheme}://{ip_address}/gopro/camera/setting?option=4&setting=91"
-    requests.get(
-        led_off_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # Disable GPS in metadata
-    gps_off_url = f"{scheme}://{ip_address}/gopro/camera/setting?option=1&setting=83"
-    requests.get(
-        gps_off_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-    # Set up auto power down to 30 minutes (option 7)
-    auto_power_down_url = f"{scheme}://{ip_address}/gopro/camera/setting?option=1&setting=59"
-    requests.get(
-        auto_power_down_url,
-        timeout=timeout,
-        verify=verify_path,
-    )
-
-
-    """Set the GoPro preset."""
-    if preset:
-        preset_url = f"{scheme}://{ip_address}/gopro/camera/presets/load?p1={preset}"
-        requests.get(
-            preset_url,
-            timeout=timeout,
-            verify=verify_path,
+    """Helper function to make HTTP requests to GoPro with common parameters."""
+    url = f"{scheme}://{ip_address}{url_path}"
+    r = requests.get(url, timeout=timeout, verify=root_ca_filepath)
+    if r.status_code != expected_response_code:
+        raise RuntimeError(
+            f"Expected response code {expected_response_code} but got {r.status_code}"
         )
+    if r.text != expected_response_text:
+        raise RuntimeError(
+            f"Expected response text {expected_response_text} but got {r.text}"
+        )
+    return r
 
 
 def _get_latest_file(
     ip_address: str,
     timeout: int,
-    verify_path: str,
+    root_ca_filepath: str,
     scheme: str,
 ):
     media_list_url = f"{scheme}://{ip_address}/gopro/media/list"
-    resp = requests.get(media_list_url, timeout=timeout, verify=verify_path)
+    resp = requests.get(media_list_url, timeout=timeout, verify=root_ca_filepath)
     resp.raise_for_status()
     data = resp.json()
 
@@ -145,7 +80,7 @@ def capture_gopro_photo(
     """
 
     scheme = "https" if root_ca else "http"
-    verify_path = True
+    root_ca_filepath = ""
     temp_file = None
     if root_ca:
         import tempfile, os
@@ -153,30 +88,127 @@ def capture_gopro_photo(
         temp_file = tempfile.NamedTemporaryFile(delete=False)
         temp_file.write(root_ca.encode())
         temp_file.close()
-        verify_path = temp_file.name
+        root_ca_filepath = temp_file.name
 
     # DONE: Implement the logic to get the last file name, capture, loop until the new file appears, download it
     # Get the last captured file before taking a new one
     latest_dir_before, latest_file_before = _get_latest_file(
-        ip_address, timeout, verify_path, scheme
+        ip_address, timeout, root_ca_filepath, scheme
     )
 
-    
-    _set_gopro_settings(ip_address, preset, timeout, verify_path, scheme)
+    """Set the GoPro settings for remote control."""
+    # Takeover control of the GoPro camera https://gopro.github.io/OpenGoPro/http#tag/Control/operation/GPCAMERA_SYSTEM_RESET
+    _make_gopro_request(
+        "/gopro/camera/control/set_ui_controller?p=2",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Set the control mode to pro
+        "/gopro/camera/setting?option=1&setting=175",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Set the LCD brightness to minimum to avoid wearing out the LCD
+    _make_gopro_request(
+        "/gopro/camera/setting?option=10&setting=88",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Set photo output to superphoto
+    _make_gopro_request(
+        "/gopro/camera/setting?option=3&setting=125",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Turn off all LEDs:
+    _make_gopro_request(
+        "/gopro/camera/setting?option=4&setting=91",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Disable GPS in metadata
+    _make_gopro_request(
+        "/gopro/camera/setting?option=1&setting=83",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Set up auto power down to 30 minutes (option 7)
+    _make_gopro_request(
+        "/gopro/camera/setting?option=1&setting=59",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # TODO(Enable this based on the GPS coordinates of the camera and expected sunset time)
+    _make_gopro_request(
+        "/gopro/camera/setting?option=1&setting=177",
+        expected_response_code=200,
+        expected_response_text="{}",
+        ip_address=ip_address,
+        timeout=timeout,
+        root_ca_filepath=root_ca_filepath,
+        scheme=scheme,
+    )
+
+    # Set the GoPro HERO 11 preset for night photography
+    if preset:
+        _make_gopro_request(
+            f"/gopro/camera/presets/load?p1={preset}",
+            expected_response_code=200,
+            expected_response_text="{}",
+            ip_address=ip_address,
+            timeout=timeout,
+            root_ca_filepath=root_ca_filepath,
+            scheme=scheme,
+        )
 
     # Trigger the shutter to capture a new photo
     trigger_url = f"{scheme}://{ip_address}/gopro/camera/shutter/start"
     requests.get(
         trigger_url,
         timeout=timeout,
-        verify=verify_path,
+        verify=root_ca_filepath,
     )
 
     # Poll for the last captured file until it changes
     while True:
         try:
             latest_dir_after, latest_file_after = _get_latest_file(
-                ip_address, timeout, verify_path, scheme
+                ip_address, timeout, root_ca_filepath, scheme
             )
             if (
                 latest_dir_after != latest_dir_before
@@ -193,7 +225,7 @@ def capture_gopro_photo(
     photo_url = (
         f"{scheme}://{ip_address}/videos/DCIM/{latest_dir_after}/{latest_file_after}"
     )
-    photo_resp = requests.get(photo_url, timeout=timeout, verify=verify_path)
+    photo_resp = requests.get(photo_url, timeout=timeout, verify=root_ca_filepath)
     photo_resp.raise_for_status()
 
     if output_file:
@@ -204,7 +236,7 @@ def capture_gopro_photo(
     requests.get(
         delete_url,
         timeout=timeout,
-        verify=verify_path,
+        verify=root_ca_filepath,
     )
 
     if temp_file:
